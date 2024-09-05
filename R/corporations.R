1cb7301edee93f75a650b4142a74d46c8c074cee
NicolasJBM <nicolas.jb.mangin@gmail.com>
2024-09-05 18:40:36 +0200

    FEAT: add list of corporations to make it independent from acanva.

---
+++ R/corporations.R
@@ -0,0 +1,19 @@
+#' @name corporations
+#' @title corporations
+#' @format A data frame with 503 rows and 10 variables
+#' \itemize{
+#'   \item  cik   character
+#'   \item  ticker   character
+#'   \item  name   character
+#'   \item  sector   character
+#'   \item  industry   character
+#'   \item  headquarter   character
+#'   \item  inSNP   character
+#'   \item  founded   character
+#'   \item  url_nyse   character
+#'   \item  url_wikipedia   character
+#' }
+#' @docType data
+#' @keywords datasets
+#' @usage data(corporations)
+NULL
